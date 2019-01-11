8639154108fac20883b6bda1d84949a7e75835e6
eselbeus <eselbeus@gmail.com>
2019-01-11 08:55:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
   end
   values.each_with_index do |num, ind|
     if num < num[ind + 1]
-      values.
+      values[ind]
   end
   name_hash.each do |name, value|
     
