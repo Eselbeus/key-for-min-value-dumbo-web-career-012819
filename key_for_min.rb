d6ef3c32eacdd0b656b8eb4fb50045ea055cb471
eselbeus <eselbeus@gmail.com>
2019-01-11 08:31:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,10 @@
 require = "pry"
 def key_for_min_value(name_hash)
   name_arr = [0]
+  binding.pry
   name_hash.each do |name, value|
     if value > name_arr[-1]
-      binding.pry
+      
       name_arr.push(name)
     elsif value < name_arr[0]
       name_arr.unshift(name)
