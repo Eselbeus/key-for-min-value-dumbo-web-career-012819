13beadca9ac465d32d5dbd342a590b53fcdfaf74
eselbeus <eselbeus@gmail.com>
2019-01-11 08:48:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,19 +5,12 @@ def key_for_min_value(name_hash)
   name_arr = []
   valueArr = []
   name_hash.each do |name, value|
-    # if value > name_arr[-1]
-    #   name_arr.push(name)
-    # elsif value < name_arr[0]
-    #   name_arr.unshift(name)
-    # end
-    valueArr.push(value)
-    compareVal = value
-    lowest = valueArr.collect do |num|
-      if compareVal < num
-        name
-      else
-        nil
-      end
+    if value > name_arr[-1]
+      name_arr.push(name)
+    elsif value < name_arr[0]
+      name_arr.unshift(name)
+    end
+    
     end
   end
   # if name_arr.length == 1 
